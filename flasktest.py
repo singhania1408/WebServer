2dc3bf527177e5e8674bbcf4929e927277c04c98
Abhishek Singhania <abhisinghania14@gmail.com>
2016-05-02 18:33:56 +0530

    BAsic Restaurant list showing

--- flasktest.py
+++ flasktest.py
@@ -2,6 +2,7 @@ from flask import Flask,render_template,request,url_for,redirect,flash,jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
+from findARestaurant import findARestaurant
 
 app = Flask(__name__)
 
@@ -25,14 +26,30 @@ def menuItemJSON(restaurant_id, menu_id):
     menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
     return jsonify(MenuItem=menuItem.serialize)
 
-@app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/menu')
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
     return render_template('basic.html',restaurant=restaurant,items=items)
 
+@app.route('/')
+@app.route('/restaurants')
+def restaurants():
+    restaurant = session.query(Restaurant)
+    return render_template('restaurantBasic.html',restaurants=restaurant)
+
 # Task 1: Create route for newMenuItem function here
+
+@app.route('/restaurants/new', methods=['GET', 'POST'])
+def restaurantNew():
+    if request.method == 'POST':
+    	newRestaurant=Restaurant(name=request.form['name'])
+    	session.add(newRestaurant)
+        session.commit()
+        flash("New Restaurant Added")
+        return redirect(url_for('restaurants'))
+    else:
+        return render_template('newRestaurant.html')
 
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
@@ -73,6 +90,23 @@ def editMenuItem(restaurant_id, menu_id):
         return render_template(
             'editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
+@app.route('/restaurants/<int:restaurant_id>/edit',
+           methods=['GET', 'POST'])
+def editRestaurant(restaurant_id):
+    editedItem = session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()        
+        flash("Restaurant Item Edited")
+        return redirect(url_for('restaurants'))
+    else:
+        # USE THE RENDER_TEMPLATE FUNCTION BELOW TO SEE THE VARIABLES YOU
+        # SHOULD USE IN YOUR EDITMENUITEM TEMPLATE
+        return render_template(
+            'editRestaurant.html', restaurant=editedItem)
+
 # Task 3: Create a route for deleteMenuItem function here
 
 
@@ -88,6 +122,17 @@ def deleteMenuItem(restaurant_id, menu_id):
     	return render_template(
     		'deletemenuitem.html', item=deletedItem)
   		
+@app.route('/restaurants/<int:restaurant_id>/delete/',methods=['GET', 'POST'])
+def deleteRestaurant(restaurant_id):
+    deletedItem=session.query(Restaurant).filter_by(id=restaurant_id).one()
+    if request.method == 'POST':
+    	session.delete(deletedItem)
+    	session.commit()
+    	flash("Menu Item Edited")    	
+    	return redirect(url_for('restaurants'))
+    else:
+    	return render_template(
+    		'deleteRestaurant.html', restaurant=deletedItem)
 
 if __name__ == '__main__':
     app.secret_key='super_secret_key'
